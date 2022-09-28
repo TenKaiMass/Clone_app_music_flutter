fc5484935bb19a19a6bba313477f4d0ae5b382fa
TenKaiMass <mass.merouane@gamil.com>
2022-09-28 15:04:03 +0200

    bdd avancement

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,9 +25,10 @@ class HomePage extends StatelessWidget {
       extendBodyBehindAppBar: true,
       body: SingleChildScrollView(
         child: Column(
-          children: [Cover(), songList()],
+          children: [songList()],
         ),
       ),
+      bottomNavigationBar: MyBottumBar(),
     );
   }
 }
@@ -35,27 +37,101 @@ class Cover extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+      height: 300,
       decoration: const BoxDecoration(
           color: Colors.greenAccent,
           image: DecorationImage(
-              image: NetworkImage(
-                  'https://m.media-amazon.com/images/I/71XeBLhY-yL._AC_SX679_.jpg'),
-                  fit: BoxFit.cover,),
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(50) 
-          )
-      ),
+            image: NetworkImage(
+                'https://m.media-amazon.com/images/I/71XeBLhY-yL._AC_SX679_.jpg'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50))),
     );
   }
 }
 
 class songList extends StatelessWidget {
+  final List musicList = [
+    {'titre': 'titre1', 'etat': true, 'nom': 'artiste'},
+    {'titre': 'titre2', 'etat': false, 'nom': 'artiste2'},
+    {'titre': 'titre1', 'etat': true, 'nom': 'artiste'},
+    {'titre': 'titre2', 'etat': false, 'nom': 'artiste2'},
+    {'titre': 'titre1', 'etat': true, 'nom': 'artiste'},
+    {'titre': 'titre2', 'etat': false, 'nom': 'artiste2'},
+    
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 500,
-      color: Colors.black38,
+      
+      padding: const EdgeInsets.fromLTRB(30, 40, 20, 20),
+      color: Colors.amber,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const SizedBox(height: 50),
+              Text(
+                'liste Musique',
+                style: GoogleFonts.aBeeZee(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500),
+              )
+            ],
+          ),
+          const SizedBox(height: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: musicList.map((song) {
+              return Container(
+                height: 70,
+                color: Colors.black12,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      song['titre'],
+                      style: GoogleFonts.actor(
+                          color:
+                              song['etat'] ? Colors.deepPurple : Colors.black,
+                          fontSize: 24),
+                    ),
+                    Text(song['nom'], style: GoogleFonts.abel(fontSize: 18)),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyBottumBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: Colors.deepPurpleAccent,
+      items: [
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.pause,color: Colors.white,),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+            label: '',
+            icon: Text(
+          'le poulet',
+          style: GoogleFonts.actor(fontSize: 14,color: Colors.white),
+        )),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.arrow_upward,color: Colors.white,),
+          label: '',
+        ),
+      ],
     );
   }
 }
